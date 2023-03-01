b9c8387a258ba8739d8a501e46ef6d0879f71eff
pete08 <pete08@gmail.com>
2023-02-28 21:16:13 -0600

    Added readme file and modifide output graph

---
+++ README.txt
@@ -0,0 +1,7 @@
+BUDGET_IT
+
+Purpose: Take back your budgeting with this easy tool that graphs past spending to analyze histoical trends.
+
+1. download past credit card statements
+2. input them into program's stmts folders
+3. run conf
--- main/plot_spend_graph.py
+++ main/plot_spend_graph.py
@@ -6,16 +6,17 @@ def plotspend(df):
     # group by Month, Year, Date
     df_group_mo_yr = df.groupby(["YearMonth"])["Amount"].sum()
     # convert columns to df
-    df_group_mo_yr = pd.DataFrame({'YearMonth':df_group_mo_yr.index, 'Amount':df_group_mo_yr.values})
+    df_group_mo_yr = pd.DataFrame({'YearMonth':df_group_mo_yr.index, '[($)]':df_group_mo_yr.values})
 
     print(df_group_mo_yr)
-    df_group_mo_yr.plot.bar("YearMonth",'Amount', rot=-70)
+    df_group_mo_yr.plot.bar("YearMonth",'[($)]', rot=-70)
     plt.xlabel('Month')
-    plt.ylabel('Amount Spent')
-    plt.title('CC Spend past Year')
-    plt.yticks(range(500,int(max(df_group_mo_yr['Amount'])+500), 500))
+    plt.ylabel('Take my Money!')
+    plt.title('Spend Tracker')
+    # plt.yticks(range(500,int(max(df_group_mo_yr['[($)]'])+500), 500))
+    plt.yticks([])
     plt.show()
 
 
-# df = pd.read_csv("./...csv")
-# plotspend(df)
+df = pd.read_csv("./spendcsv_2022-12-07 21:25.csv")
+plotspend(df)
